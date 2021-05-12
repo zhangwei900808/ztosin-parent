3a7dbd298d1462a4b251249d6b5e7f4b1e49bb19
justinzhang <wei.zhanga@einyun.com>
2021-05-12 17:02:27 +0800

    init project

---
+++ ztosin-nacossvr/src/main/java/com/ztosin/nacossvr/NacosServerApplication.java
@@ -0,0 +1,10 @@
+package com.ztosin.nacossvr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class, args);
+    }
+}
